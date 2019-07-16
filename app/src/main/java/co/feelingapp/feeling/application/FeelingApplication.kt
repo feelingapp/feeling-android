aee1a45077574a547aa38cbc844b7ef4a55ed8b1
Adam Cox <adamjhcox@gmail.com>
2019-07-16 18:23:58 +0100

    Actually both should be the base interface

--- app/src/main/java/co/feelingapp/feeling/application/FeelingApplication.kt
+++ app/src/main/java/co/feelingapp/feeling/application/FeelingApplication.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.Application
 import androidx.fragment.app.Fragment
 import co.feelingapp.feeling.injection.DaggerAppComponent
+import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
 import dagger.android.support.HasSupportFragmentInjector
@@ -22,11 +23,11 @@ class FeelingApplication : Application(), HasActivityInjector, HasSupportFragmen
         DaggerAppComponent.create().inject(this)
     }
 
-    override fun activityInjector(): DispatchingAndroidInjector<Activity> {
+    override fun activityInjector(): AndroidInjector<Activity> {
         return activityInjector
     }
 
-    override fun supportFragmentInjector(): DispatchingAndroidInjector<Fragment> {
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
         return fragmentInjector
     }
 }
