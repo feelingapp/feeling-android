a2e16062faaa04426f8da75897a9d91dcc9d8629
Pav Sidhu <pav@pavsidhu.com>
2019-07-20 22:07:39 +0100

    Fix imports

--- app/src/main/java/app/getfeeling/feeling/injection/module/MainAppModule.kt
+++ app/src/main/java/app/getfeeling/feeling/injection/module/MainAppModule.kt
@@ -2,9 +2,15 @@ package app.getfeeling.feeling.injection.module
 
 import android.app.Application
 import androidx.room.Room
+import app.getfeeling.feeling.BuildConfig
+import app.getfeeling.feeling.api.FeelingService
 import app.getfeeling.feeling.room.FeelingDatabase
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -18,7 +24,7 @@ class AppModule {
 
     @Provides
     @Singleton
-    fun provideFeelingService(): Retrofit {
+    fun provideFeelingService(): FeelingService {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.FEELING_API_URL)
             .addConverterFactory(MoshiConverterFactory.create())
