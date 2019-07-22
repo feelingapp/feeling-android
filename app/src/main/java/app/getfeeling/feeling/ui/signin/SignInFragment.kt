050bb4c834af60f567409231b57b5d02439d1a00
Pav Sidhu <pav@pavsidhu.com>
2019-07-22 21:40:22 +0100

    Add sign in screen

---
+++ app/src/main/java/app/getfeeling/feeling/ui/signin/SignInFragment.kt
@@ -0,0 +1,33 @@
+package app.getfeeling.feeling.ui.signin
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.getfeeling.feeling.R
+
+
+class SignInFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SignInFragment()
+    }
+
+    private lateinit var viewModel: SignInViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.sign_in_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(SignInViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/app/getfeeling/feeling/ui/signin/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package app.getfeeling.feeling.ui.signin
+
+import androidx.lifecycle.ViewModel;
+
+class SignInViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
