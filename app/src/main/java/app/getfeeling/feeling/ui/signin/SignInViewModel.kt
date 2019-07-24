0d233ffc01b646a2e83db92cf8ce593708c3ab86
Pav Sidhu <pav@pavsidhu.com>
2019-07-24 13:09:13 +0100

    Go to browser to authenticate

--- app/src/main/java/app/getfeeling/feeling/ui/signin/SignInViewModel.kt
+++ app/src/main/java/app/getfeeling/feeling/ui/signin/SignInViewModel.kt
@@ -6,17 +6,35 @@ import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.browser.customtabs.TrustedWebUtils
 import androidx.lifecycle.AndroidViewModel
+import app.getfeeling.feeling.BuildConfig
 
 
 class SignInViewModel(application: Application) : AndroidViewModel(application) {
     fun continueWithEmail() {
-        val customTabsIntent = CustomTabsIntent.Builder().build()
+        val context = getApplication<Application>().applicationContext
+
+        val customTabsIntent = CustomTabsIntent.Builder()
+            .build()
         customTabsIntent.intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
 
+        val uri = Uri.Builder()
+            .scheme("https")
+            .authority("getfeeling.app")
+            .appendPath("/authorize")
+            .appendQueryParameter("client_id", BuildConfig.FEELING_API_CLIENT_ID)
+            .appendQueryParameter("response_type", "code")
+            .appendQueryParameter("redirect_uri", BuildConfig.FEELING_API_REDIRECT_URI)
+            .appendQueryParameter("code_challenge_method", "")
+            .appendQueryParameter("code_challenge", "")
+            .appendQueryParameter("code_challenge_hash", "")
+            .build()
+
+        print(uri)
+
         TrustedWebUtils.launchAsTrustedWebActivity(
-            getApplication<Application>().applicationContext,
+            context,
             customTabsIntent,
-            Uri.parse("https://getfeeling.app/authorize")
+            uri
         )
     }
 }
