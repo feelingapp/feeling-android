4ace51c84cb6d1b8cfb54df2fc2fb0ce89e8a508
Adam Cox <adamjhcox@gmail.com>
2019-08-03 13:08:49 +0100

    Added a Converter for handling error responses

--- app/src/main/java/app/getfeeling/feeling/repository/FeelingRepository.kt
+++ app/src/main/java/app/getfeeling/feeling/repository/FeelingRepository.kt
@@ -3,15 +3,20 @@ package app.getfeeling.feeling.repository
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.liveData
 import app.getfeeling.feeling.api.FeelingService
+import app.getfeeling.feeling.api.models.ErrorsModel
 import app.getfeeling.feeling.repository.interfaces.IFeelingRepository
 import app.getfeeling.feeling.room.dao.FeelingDao
 import kotlinx.coroutines.Dispatchers
+import okhttp3.ResponseBody
+import retrofit2.Converter
 import javax.inject.Inject
+
 
 @WorkerThread
 class FeelingRepository @Inject constructor(
     private val feelingDao: FeelingDao,
-    private val feelingService: FeelingService
+    private val feelingService: FeelingService,
+    private val errorConverter: Converter<ResponseBody, ErrorsModel>
 ) : IFeelingRepository {
 
     override fun getStatus() = liveData(Dispatchers.IO) {
