7da3aed7ca66ef968d4cb577560149ab2449b6b9
Pav Sidhu <pav@pavsidhu.com>
2019-08-15 21:55:07 +0100

    Add code challenge and state to sign in redirect link

--- app/src/main/java/app/getfeeling/feeling/ui/signin/SignInViewModel.kt
+++ app/src/main/java/app/getfeeling/feeling/ui/signin/SignInViewModel.kt
@@ -7,7 +7,7 @@ import androidx.browser.customtabs.CustomTabsIntent
 import androidx.browser.customtabs.TrustedWebUtils
 import androidx.lifecycle.AndroidViewModel
 import app.getfeeling.feeling.BuildConfig
-
+import app.getfeeling.feeling.util.PKCE
 
 class SignInViewModel(application: Application) : AndroidViewModel(application) {
     fun continueWithEmail() {
@@ -17,24 +17,32 @@ class SignInViewModel(application: Application) : AndroidViewModel(application) 
             .build()
         customTabsIntent.intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
 
+        val pkce = PKCE()
+        val codeVerifier = pkce.generateCodeVerifier()
+        val codeChallenge = pkce.generateCodeChallenge(codeVerifier)
+        val state = generateState()
+
         val uri = Uri.Builder()
-            .scheme("https")
-            .authority("getfeeling.app")
-            .appendPath("/authorize")
+            .scheme("http")
+            .authority(BuildConfig.FEELING_WEBSITE_URL)
+            .appendPath("authorize")
             .appendQueryParameter("client_id", BuildConfig.FEELING_API_CLIENT_ID)
             .appendQueryParameter("response_type", "code")
             .appendQueryParameter("redirect_uri", BuildConfig.FEELING_API_REDIRECT_URI)
-            .appendQueryParameter("code_challenge_method", "")
-            .appendQueryParameter("code_challenge", "")
-            .appendQueryParameter("code_challenge_hash", "")
+            .appendQueryParameter("code_challenge_method", PKCE.CODE_CHALLENGE_METHOD)
+            .appendQueryParameter("code_challenge", codeChallenge)
+            .appendQueryParameter("state", state)
             .build()
-
-        print(uri)
 
         TrustedWebUtils.launchAsTrustedWebActivity(
             context,
             customTabsIntent,
             uri
         )
+    }
+
+    private fun generateState(): String {
+        val stateCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toList().toTypedArray()
+        return (1..32).map { stateCharacters.random() }.joinToString("")
     }
 }
