fce266a93ed62a25ee4df1a3b00dfa8e1ca390ba
Pav Sidhu <pav@pavsidhu.com>
2019-08-20 21:13:50 +0100

    Partial navigation solution

--- app/src/main/java/app/getfeeling/feeling/MainActivity.kt
+++ app/src/main/java/app/getfeeling/feeling/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import app.getfeeling.feeling.ui.signin.SignInViewModel
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.android.AndroidInjection
 import javax.inject.Inject
 
@@ -18,34 +15,18 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    private lateinit var navController: NavController
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var mainNavController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        navController = findNavController(R.id.nav_host_fragment)
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setOnNavigationItemReselectedListener { }
-        bottomNavigationView.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.action_settings -> navigate(R.id.action_main_fragment_to_settings_fragment)
-                R.id.action_me -> navigate(R.id.action_settings_fragment_to_main_fragment)
-            }
-            true
-        }
+        mainNavController = findNavController(R.id.nav_host_fragment)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration)
-    }
-
-    private fun navigate(resId: Int) {
-        navController.navigate(resId)
+    fun navigate(resId: Int) {
+        mainNavController.navigate(resId)
     }
 
     override fun onNewIntent(intent: Intent) {
