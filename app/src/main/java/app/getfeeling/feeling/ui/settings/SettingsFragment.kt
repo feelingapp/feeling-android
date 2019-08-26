44e350ec0d4d64ba77423dc688781b3390b5e7d3
Pav Sidhu <pav@pavsidhu.com>
2019-08-26 21:12:06 +0100

    Open terms and privacy from settings

--- app/src/main/java/app/getfeeling/feeling/ui/settings/SettingsFragment.kt
+++ app/src/main/java/app/getfeeling/feeling/ui/settings/SettingsFragment.kt
@@ -1,6 +1,9 @@
 package app.getfeeling.feeling.ui.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.preference.Preference
@@ -18,10 +21,19 @@ class SettingsFragment : PreferenceFragmentCompat(), PreferenceManager.OnPrefere
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
         when (preference.key) {
-            "sign_out" -> mainNavController?.navigate(R.id.action_settings_fragment_to_sign_in_fragment)
+            "terms_and_conditions" -> openPage("https://getfeeling.app/terms")
+            "privacy_policy" -> openPage("https://getfeeling.app/privacy")
             "daily_reminder" -> mainNavController?.navigate(R.id.action_settings_fragment_to_daily_reminder_fragment)
+            "sign_out" -> mainNavController?.navigate(R.id.action_settings_fragment_to_sign_in_fragment)
         }
 
         return super.onPreferenceTreeClick(preference)
+    }
+
+    private fun openPage(uri: String) {
+        val customTabsIntent = CustomTabsIntent.Builder().build()
+        customTabsIntent.intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+        customTabsIntent.launchUrl(context, Uri.parse(uri))
     }
 }
