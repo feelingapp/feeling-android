6d2b7da6fe8ad8f8ec19042b533f29d9b593252c
Pav Sidhu <pav@pavsidhu.com>
2019-08-26 16:40:33 +0100

    Add title to toolbar

--- app/src/main/java/app/getfeeling/feeling/ui/me/MeFragment.kt
+++ app/src/main/java/app/getfeeling/feeling/ui/me/MeFragment.kt
@@ -1,35 +1,39 @@
 package app.getfeeling.feeling.ui.me
 
-import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import app.getfeeling.feeling.R
-import dagger.android.support.AndroidSupportInjection
+import androidx.lifecycle.ViewModelProviders
+import app.getfeeling.feeling.databinding.MeFragmentBinding
+import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
-class MeFragment : Fragment() {
+class MeFragment : DaggerFragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private lateinit var viewModel: MeViewModel
 
-    override fun onAttach(context: Context) {
-        AndroidSupportInjection.inject(this)
-        super.onAttach(context)
-    }
+    private lateinit var binding: MeFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.me_fragment, container, false)
+        binding = MeFragmentBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewModel = ViewModelProviders.of(activity!!, viewModelFactory).get(MeViewModel::class.java)
+
+        binding.viewModel = viewModel
     }
 }
--- app/src/main/java/app/getfeeling/feeling/ui/me/MeViewModel.kt
+++ app/src/main/java/app/getfeeling/feeling/ui/me/MeViewModel.kt
@@ -1,20 +1,9 @@
 package app.getfeeling.feeling.ui.me
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import app.getfeeling.feeling.repository.interfaces.IFeelingRepository
 import javax.inject.Inject
 
 class MeViewModel @Inject constructor(private val repository: IFeelingRepository) : ViewModel() {
-
-    private val input = MutableLiveData<Boolean>()
-    var isApiOnline: LiveData<String> = Transformations.switchMap(input) {
-        repository.getStatus()
-    }
-
-    fun setInput() {
-        input.value = input.value?.not()
-    }
+    fun getTitle(): String = "Welcome Back Michael"
 }
