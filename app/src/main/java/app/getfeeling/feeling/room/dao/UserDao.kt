9409d2bbdc6043ab34f88bab0373aef763c1dc19
Pav Sidhu <pav@pavsidhu.com>
2019-09-15 14:57:46 +0100

    Combine User network and room models into one

--- app/src/main/java/app/getfeeling/feeling/room/dao/UserDao.kt
+++ app/src/main/java/app/getfeeling/feeling/room/dao/UserDao.kt
@@ -4,7 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import app.getfeeling.feeling.room.entities.User
+import app.getfeeling.feeling.models.User
 
 @Dao
 interface UserDao {
--- app/src/main/java/app/getfeeling/feeling/room/entities/Feeling.kt
+++ app/src/main/java/app/getfeeling/feeling/room/entities/Feeling.kt
@@ -1,6 +1,7 @@
 package app.getfeeling.feeling.room.entities
 
 import androidx.room.*
+import app.getfeeling.feeling.models.User
 import org.threeten.bp.OffsetDateTime
 import org.threeten.bp.YearMonth
 
