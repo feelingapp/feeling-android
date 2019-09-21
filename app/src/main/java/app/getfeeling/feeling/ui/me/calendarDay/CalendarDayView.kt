9688252def2393acd0204cd5ebc6072df7e0b31c
Adam Cox <adamjhcox@gmail.com>
2019-09-21 20:53:24 +0100

    Changed CalendarDayView to an ImageView
    This avoids click listener complications as grid view already it's own item click listener so there is no need for a button

--- app/src/main/java/app/getfeeling/feeling/ui/me/calendarDay/CalendarDayView.kt
+++ app/src/main/java/app/getfeeling/feeling/ui/me/calendarDay/CalendarDayView.kt
@@ -2,9 +2,9 @@ package app.getfeeling.feeling.ui.me.calendarDay
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.ImageButton
+import android.widget.ImageView
 
-class CalendarDayView(context: Context, attrs: AttributeSet) : ImageButton(context, attrs) {
+class CalendarDayView(context: Context, attrs: AttributeSet) : ImageView(context, attrs) {
 
     public override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         // To make sure grid cell is square
